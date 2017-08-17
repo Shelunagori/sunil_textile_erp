21a5de1abf209aaa80817e93d9dc2d25e176b2b4
manojTanwar <manojtanwar953@gmail.com>
2017-08-17 14:09:38 +0530

    yughjg

---
+++ src/Controller/CustomersController.php
@@ -0,0 +1,137 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Customers Controller
+ *
+ * @property \App\Model\Table\CustomersTable $Customers
+ *
+ * @method \App\Model\Entity\Customer[] paginate($object = null, array $settings = [])
+ */
+class CustomersController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index($id=null)
+    {
+		$this->viewBuilder()->layout('index_layout');
+		/* if(!empty($id))
+		{
+			$customer = $this->Customers->get($id, [
+				'contain' => ['States']
+			]);
+		}
+		else{
+			$customer = $this->Customers->patchEntity();
+		} */
+		
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $customer = $this->Customers->patchEntity($customer, $this->request->getData());
+            if ($this->Customers->save($customer)) {
+                $this->Flash->success(__('The customer has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The customer could not be saved. Please, try again.'));
+        }
+		
+        $this->paginate = [
+            'contain' => ['States']
+        ];
+        $customers = $this->paginate($this->Customers);
+
+        $this->set(compact('customers','customer'));
+        $this->set('_serialize', ['customers']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Customer id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $customer = $this->Customers->get($id, [
+            'contain' => ['States', 'Ledgers']
+        ]);
+
+        $this->set('customer', $customer);
+        $this->set('_serialize', ['customer']);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $customer = $this->Customers->newEntity();
+        if ($this->request->is('post')) {
+            $customer = $this->Customers->patchEntity($customer, $this->request->getData());
+            if ($this->Customers->save($customer)) {
+                $this->Flash->success(__('The customer has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The customer could not be saved. Please, try again.'));
+        }
+        $states = $this->Customers->States->find('list', ['limit' => 200]);
+        $this->set(compact('customer', 'states'));
+        $this->set('_serialize', ['customer']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Customer id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $customer = $this->Customers->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $customer = $this->Customers->patchEntity($customer, $this->request->getData());
+            if ($this->Customers->save($customer)) {
+                $this->Flash->success(__('The customer has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The customer could not be saved. Please, try again.'));
+        }
+        $states = $this->Customers->States->find('list', ['limit' => 200]);
+        $this->set(compact('customer', 'states'));
+        $this->set('_serialize', ['customer']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Customer id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $customer = $this->Customers->get($id);
+        if ($this->Customers->delete($customer)) {
+            $this->Flash->success(__('The customer has been deleted.'));
+        } else {
+            $this->Flash->error(__('The customer could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ src/Model/Table/CustomersTable.php
@@ -0,0 +1,101 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Customers Model
+ *
+ * @property \App\Model\Table\StatesTable|\Cake\ORM\Association\BelongsTo $States
+ * @property \App\Model\Table\LedgersTable|\Cake\ORM\Association\HasMany $Ledgers
+ *
+ * @method \App\Model\Entity\Customer get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Customer newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Customer[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Customer|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Customer patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Customer[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Customer findOrCreate($search, callable $callback = null, $options = [])
+ */
+class CustomersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('customers');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('States', [
+            'foreignKey' => 'state_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->hasMany('Ledgers', [
+            'foreignKey' => 'customer_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->requirePresence('gstin', 'create')
+            ->notEmpty('gstin');
+
+        $validator
+            ->email('email')
+            ->allowEmpty('email');
+
+        $validator
+            ->allowEmpty('mobile');
+
+        $validator
+            ->allowEmpty('address');
+
+        $validator
+            ->boolean('freeze')
+            ->requirePresence('freeze', 'create')
+            ->notEmpty('freeze');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['email']));
+        $rules->add($rules->existsIn(['state_id'], 'States'));
+
+        return $rules;
+    }
+}
